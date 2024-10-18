dc2319f6bd41d497354650d07c10bde939e4a316
Dominic Anuta Vega <davega12.dv@gmail.com>
2024-10-18 10:21:38 +0800

    friendly matches

---
+++ src/app/admin/pages/leagues/league-details/friendly-matches/friendly-matches.component.ts
@@ -0,0 +1,170 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MatchService } from '../../../../../services/match.service';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../../../../services/api.service';
+import { DatePipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ScoreUpdateService } from '../../../../../services/score-league.service';
+import { FriendlyMatchAddEditComponent } from './friendly-match-add-edit/friendly-match-add-edit.component';
+
+@Component({
+  selector: 'app-friendly-matches',
+  standalone: true,
+  imports: [
+    MatButtonModule, 
+    MatTableModule, 
+    CommonModule, 
+    MatIconModule, 
+    MatTooltipModule
+  ],
+  providers: [
+    DatePipe,
+  ],
+  templateUrl: './friendly-matches.component.html',
+  styleUrl: './friendly-matches.component.sass'
+})
+export class FriendlyMatchesComponent {
+  leagueId: number | null = null;
+  displayedColumns: string[] = ['match_date', 'match_time', 'location', 'for', 'result', 'against', 'status', 'action'];
+  dataSource!: MatTableDataSource<any>;
+  imagePath: string | null = null;
+
+  constructor(
+    private dialog: MatDialog, 
+    private route: ActivatedRoute,
+    private matchesService: MatchService,
+    private scoreUpdateService: ScoreUpdateService,
+    private _configService: ApiService,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.leagueId = +params['id'];  
+      this.getFriendlyMatches(this.leagueId);
+    });
+    this.imagePath = `${this._configService.URL_IMAGE}`;
+  }
+
+  openEditAdd() {
+    const dialogRef = this.dialog.open(FriendlyMatchAddEditComponent, {
+      data: { leagueId: this.leagueId }
+    });
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val && this.leagueId) {
+          this.getFriendlyMatches(this.leagueId)
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  
+  getFriendlyMatches(leagueId: number) {
+    this.matchesService.getMatches(leagueId).subscribe({
+      next: (res: any[]) => {
+        res.forEach(match => {
+          match.matchTime = new Date(`2000-01-01T${match.match_time}`);
+        });
+        res.sort((a, b) => new Date(a.match_date).getTime() - new Date(b.match_date).getTime());
+        this.dataSource = new MatTableDataSource(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  // updateScore(element: any, index: number, id: number, team_id: number, points: number, result: string) {
+  //   const dialogRef = this.dialog.open(TeamScoreComponent, {
+  //       disableClose: true,
+  //       data: { 
+  //         score_id: id,
+  //         team_id: team_id,
+  //         leagueId: this.leagueId, 
+  //         matchId: element.id,
+  //         points: points,
+  //         result: result
+  //       }
+  //   });
+  //   dialogRef.afterClosed().subscribe({
+  //     next: (val) => {
+  //       if (val && this.leagueId) {
+  //         this.getMatches(this.leagueId)
+  //         this.scoreUpdateService.notifyScoreUpdated();
+  //       }
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
+  //     }
+  //   })
+  // }
+
+  // updateTeam(element: any, index: number, id: number, team_id: number) {
+  //   const dialogRef = this.dialog.open(TeamSelectComponent, {
+  //       data: { 
+  //         score_id: id,
+  //         team_id: team_id,
+  //         leagueId: this.leagueId, 
+  //         matchId: element.id 
+  //       }
+  //   });
+  //   dialogRef.afterClosed().subscribe({
+  //     next: (val) => {
+  //       if (val && this.leagueId) {
+  //         this.getMatches(this.leagueId)
+  //       }
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
+  //     }
+  //   })
+  // }
+
+  // addTeam(element: any, index: number) {
+  //   const dialogRef = this.dialog.open(TeamSelectComponent, {
+  //       data: { 
+  //         leagueId: this.leagueId, 
+  //         matchId: element.id 
+  //       }
+  //   });
+  //   dialogRef.afterClosed().subscribe({
+  //     next: (val) => {
+  //       if (val && this.leagueId) {
+  //         this.getMatches(this.leagueId)
+  //       }
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
+  //     }
+  //   })
+  // }
+  
+
+  
+
+  // openEditFormMatch(data: any) {
+  //   const dialogRef = this.dialog.open(TeamMatchAddEditComponent, {
+  //     data,
+  //   });
+
+  //   dialogRef.afterClosed().subscribe({
+  //       next: (val) => {
+  //         if (val && this.leagueId) {
+  //           this.getMatches(this.leagueId)
+  //         }
+  //       },
+  //       error: (err) => {
+  //         console.log(err);
+  //       }
+  //     })
+  // }
+}
---
+++ src/app/services/friendly-match.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendlyMatchService {
+
+  constructor(private _http: HttpClient) { }
+
+  addFriendlyMatch(data: any): Observable<any> {
+    return this._http.post(environment.apiUrl+`/football/friendly-matches`, data);
+  }
+
+  updateFriendlyMatch(id: number, data: any): Observable<any> {
+    return this._http.patch(environment.apiUrl+`/football/friendly-matches/${id}`, data);
+  }
+
+  getFriendlyMatchById(id: number): Observable<any> {
+    return this._http.get(environment.apiUrl + `/football/friendly-matches/${id}`);
+  }
+
+  getFriendlyMatches(): Observable<any> {
+    return this._http.get(environment.apiUrl+`/football/friendly-matches`);
+  }
+
+  deleteFriendlyMatch(id: number): Observable<any> {
+    return this._http.delete(environment.apiUrl+`/football/friendly-matches/${id}`)
+  }
+}
