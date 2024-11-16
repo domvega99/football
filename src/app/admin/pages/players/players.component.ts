a984bac8c80ba07f7f61a084c5cddd54277a59ce
Dominic Anuta Vega <davega12.dv@gmail.com>
2024-11-16 11:00:04 +0800

    Admin - Players

--- src/app/admin/pages/players/players.component.ts
+++ src/app/admin/pages/players/players.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -9,33 +9,32 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { RouterLink, Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CoreService } from '../../../core/core.service';
 import { ApiService } from '../../../services/api.service';
 import { SquadService } from '../../../services/squad.service';
-import { PlayerAddEditComponent } from './player-add-edit/player-add-edit.component';
 
 @Component({
   selector: 'app-players',
   standalone: true,
   imports: [
-    RouterLink, 
-    CommonModule, 
-    MatButtonModule, 
-    MatDialogModule, 
-    MatFormFieldModule, 
-    MatInputModule, 
-    MatTableModule, 
-    MatSortModule, 
-    MatPaginatorModule, 
-    MatIconModule, 
+    RouterLink,
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatIconModule,
     MatSnackBarModule
   ],
   templateUrl: './players.component.html',
   styleUrl: './players.component.sass'
 })
 export class PlayersComponent {
-  displayedColumns: string[] = ['first_name', 'last_name', 'position', 'stat', 'action'];
+  displayedColumns: string[] = ['playerNumber', 'first_name', 'last_name', 'position', 'club', 'stat', 'action'];
   teamId: number | null = null;
   dataSource!: MatTableDataSource<any>;
   imagePath: string | null = null;
@@ -44,15 +43,14 @@ export class PlayersComponent {
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
-    private _dialog: MatDialog, 
     private squadService: SquadService,
-    private _coreService: CoreService,
-    private _configService: ApiService,
+    private coreService: CoreService,
+    private configService: ApiService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.imagePath =`${this._configService.URL_IMAGE}`;
+    this.imagePath =`${this.configService.URL_IMAGE}`;
     this.getPlayers();
   }
 
@@ -72,27 +70,10 @@ export class PlayersComponent {
   openEditPlayer(data: any) {
     this.router.navigate(['/admin/players/edit', data.id]);
   }
-  
-  openAddEditTeamForm() {
-    const dialogRef = this._dialog.open(PlayerAddEditComponent, {
-      data: { teamId: this.teamId }
-    });
-    dialogRef.afterClosed().subscribe({
-      next: (val) => {
-        if (val && this.teamId) {
-          this.getPlayers();
-        }
-      },
-      error: (err) => {
-        console.log(err);
-      }
-    })
-  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
@@ -101,30 +82,13 @@ export class PlayersComponent {
   deleteSquad(id: number) {
     this.squadService.deleteSquad(id).subscribe({
       next: (res) => {
-        this._coreService.openSnackBar('Player deleted successfully')
+        this.coreService.openSnackBar('Player deleted successfully')
         this.getPlayers();
       },
       error: (err) => {
         console.log(err);
       }
     })
-  }
-
-  openEditForm(data: any) {
-    const dialogRef = this._dialog.open(PlayerAddEditComponent, {
-      data,
-    });
-
-    dialogRef.afterClosed().subscribe({
-        next: (val) => {
-          if (val && this.teamId) {
-            this.getPlayers();
-          }
-        },
-        error: (err) => {
-          console.log(err);
-        }
-      })
   }
 }
 
