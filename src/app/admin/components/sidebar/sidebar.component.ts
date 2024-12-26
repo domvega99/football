e4075cd613a84c916d8fa2476b7d7f571068a6e7
Dominic Anuta Vega <davega12.dv@gmail.com>
2024-12-26 09:41:45 +0800

    Website - Sidebar

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -1,43 +1,49 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
-import { UserService } from '../../../services/user.service';
 
 interface MenuItem {
   label: string;
   icon: string;
   routerLink: string;
   roles: string[];
+  group: string;
 }
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ RouterModule, MatListModule, MatIconModule, CommonModule],
+  imports: [ 
+    RouterModule,
+    MatListModule,
+    MatIconModule,
+    CommonModule
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.sass'
+  styleUrls: ['./sidebar.component.sass']
 })
 export class SidebarComponent {
 
-  role: string | null = null
+  role: string | null = null;
   isAuthenticated: boolean = false;
   user: any;
+  groupedMenuItems: { [key: string]: MenuItem[] } = {};
+  
+  groupOrder: string[] = ['Main', 'Competition', 'Football', 'Management', 'Content'];
 
   constructor(
-    private authService: AuthService,
-    private userService: UserService
-  ) {}
-
-
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
-    const userData = this.authService.getUser()
+    const userData = this.authService.getUser();
     if (userData) {
       this.role = userData.role;
-      this.user = userData
+      this.user = userData;
+      this.groupedMenuItems = this.getGroupedMenuItems();
     }
   }
 
@@ -47,53 +53,137 @@ export class SidebarComponent {
   }
 
   menuItems: MenuItem[] = [
-  {
-    label: 'Dashboard',
-    icon: 'dashboard',
-    routerLink: '/admin/dashboard',
-    roles: ['Super Admin', 'Admin', 'Content Editor', 'Team']
-  },
-  // {
-  //   label: 'Post',
-  //   icon: 'store',
-  //   routerLink: '/admin/posts',
-  //   roles: ['Super Admin', 'Admin', 'Content Editor']
-  // },
-  {
-    label: 'Teams',
-    icon: 'flag',
-    routerLink: '/admin/teams',
-    roles: ['Super Admin', 'Admin', 'Team', 'Content Editor']
-  },
-  {
-    label: 'Leagues',
-    icon: 'golf_course',
-    routerLink: '/admin/leagues',
-    roles: ['Super Admin', 'Admin']
-  },
-  {
-    label: 'Contents',
-    icon: 'new_releases',
-    routerLink: '/admin/contents',
-    roles: ['Super Admin', 'Admin', 'Content Editor']
-  },
-  {
-    label: 'Pages',
-    icon: 'new_releases',
-    routerLink: '/admin/pages',
-    roles: ['Super Admin', 'Admin', 'Content Editor']
-  },
-  {
-    label: 'Club Locators',
-    icon: 'flag',
-    routerLink: '/admin/club-locators',
-    roles: ['Super Admin', 'Admin', 'Team', 'Content Editor']
-  },
-  {
-    label: 'Users',
-    icon: 'group',
-    routerLink: '/admin/users',
-    roles: ['Super Admin', 'Admin']
-  }
+    // Main
+    { 
+      label: 'Dashboard', 
+      icon: 'dashboard', 
+      routerLink: '/admin/dashboard', 
+      roles: ['Super Admin', 'Admin', 'Content Editor', 'Team'], 
+      group: 'Main' 
+    },
+    // Football
+    { 
+      label: 'Club Locators', 
+      icon: 'sports_soccer', 
+      routerLink: '/admin/club-locators', 
+      roles: ['Super Admin', 'Admin', 'Content Editor'], 
+      group: 'Football' 
+    },
+    { 
+      label: 'Teams', 
+      icon: 'flag', 
+      routerLink: '/admin/teams', 
+      roles: ['Super Admin', 'Admin', 'Content Editor'], 
+      group: 'Football' 
+    },
+    // { 
+    //   label: 'Tiers', 
+    //   icon: 'sports_soccer', 
+    //   routerLink: '/admin/tiers', 
+    //   roles: ['Super Admin', 'Admin', 'Team', 'Content Editor'], 
+    //   group: 'Football' 
+    // },
+    // { 
+    //   label: 'Football Years', 
+    //   icon: 'sports_soccer', 
+    //   routerLink: '/admin/years', 
+    //   roles: ['Super Admin', 'Admin', 'Team', 'Content Editor'], 
+    //   group: 'Football' 
+    // },
+    // Competition
+    { 
+      label: 'Leagues', 
+      icon: 'sports_score', 
+      routerLink: '/admin/leagues', 
+      roles: ['Super Admin', 'Admin'], 
+      group: 'Competition' 
+    },
+    // { 
+    //   label: 'Cups', 
+    //   icon: 'sports_score', 
+    //   routerLink: '/admin/cups', 
+    //   roles: ['Super Admin', 'Admin'], 
+    //   group: 'Competition' 
+    // },
+    // { 
+    //   label: 'Friendly Matches', 
+    //   icon: 'sports_score', 
+    //   routerLink: '/admin/friendly-matches', 
+    //   roles: ['Super Admin', 'Admin'], 
+    //   group: 'Competition' 
+    // },
+    // Management
+    // { 
+    //   label: 'Associations', 
+    //   icon: 'spoke', 
+    //   routerLink: '/admin/associations', 
+    //   roles: ['Super Admin', 'Admin', 'Content Editor'], 
+    //   group: 'Management' 
+    // },
+    // { 
+    //   label: 'Players', 
+    //   icon: 'group', 
+    //   routerLink: '/admin/players', 
+    //   roles: ['Super Admin', 'Admin', 'Content Editor'], 
+    //   group: 'Management' 
+    // },
+    // { 
+    //   label: 'Coaches', 
+    //   icon: 'group', 
+    //   routerLink: '/admin/coaches', 
+    //   roles: ['Super Admin', 'Admin', 'Content Editor'], 
+    //   group: 'Management' 
+    // },
+    { 
+      label: 'Users', 
+      icon: 'group', 
+      routerLink: '/admin/users', 
+      roles: ['Super Admin', 'Admin'], 
+      group: 'Management' 
+    },
+    // Content
+    { 
+      label: 'Contents', 
+      icon: 'new_releases', 
+      routerLink: '/admin/contents', 
+      roles: ['Super Admin', 'Admin', 'Content Editor'], 
+      group: 'Content' 
+    },
+    { 
+      label: 'Pages', 
+      icon: 'new_releases', 
+      routerLink: '/admin/pages', 
+      roles: ['Super Admin', 'Admin', 'Content Editor'], 
+      group: 'Content' 
+    },
   ];
+
+  getGroupedMenuItems() {
+    const groups = this.menuItems.reduce(
+      (acc: { [key: string]: MenuItem[] }, item: MenuItem) => {
+        const group = item.group || 'Others';
+        if (!acc[group]) {
+          acc[group] = [];
+        }
+        acc[group].push(item);
+        return acc;
+      },
+      {}
+    );
+
+    const sortedGroups: { [key: string]: MenuItem[] } = {};
+    this.groupOrder.forEach((group) => {
+      if (groups[group]) {
+        sortedGroups[group] = groups[group];
+      }
+    });
+
+    Object.keys(groups).forEach((group) => {
+      if (!sortedGroups[group]) {
+        sortedGroups[group] = groups[group];
+      }
+    });
+
+    return sortedGroups;
+  }
 }
